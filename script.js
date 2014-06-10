f7ee8df1fd7179cff86c405e8abe05fae638c6a2
Daniel Wood <daniel.j.wood11@gmail.com>
2014-06-10 18:19:03 -0400

    added js to change description on hover

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 // Initial Script
 
+var content = document.getElementById('r-n-sc-info2');
+var text = "";
+var title = "";
 
 (function ($) { 
+
 
   $(document).ready(function() { 
       console.log('hello')
 
-    $('.parts').bind('touchstart touchend', function(e) {
+    // $('.parts').bind('touchstart touchend', function(e) {
+    //     e.preventDefault();
+    //     $(this).toggleClass('hover_effect');
+    //     console.log('hover')
+    // });
+
+
+
+    $('.parts').on( "mouseover", function(e) {
         e.preventDefault();
         $(this).toggleClass('hover_effect');
-        console.log('hover')
+        layer = $(this).attr('data-name')
+        // console.log(layer)
+
+        for (var i = 0; i < data.length; i++) {
+
+          if (layer == data[i].cls) {
+            var text = data[i].description; 
+            var title = data[i].name;
+          };  
+        };
+
+        content.innerHTML = "<center><h4>" + title + "</h4></center>" + 
+                            "<p>" + text + "</p>";
     });
 
-    // $( "#anemometer" ).mouseover(
-      // console.log('hover')
-      // console.log(this)
-
-    // );
-
-  // $('.hover').bind('touchstart touchend', function(e) {
-  //       e.preventDefault();
-  //       $(this).toggleClass('hover_effect');
-  //   });
 
 
 
