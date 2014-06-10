5aa6c49d216c7832c076baec6bf10d9e6aa4e263
Daniel Wood <daniel.j.wood11@gmail.com>
2014-06-10 17:40:03 -0400

    added data, made some changes

---
+++ data1.js
@@ -0,0 +1,98 @@
+var data = [
+  {
+    "id":"1",
+    "cls":"anemometer",
+    "name":"Anemometer",
+    "description":"Measures the wind speed and transmits wind speed data to the controller."
+  },
+  {
+    "id":"1",
+    "cls":"blades",
+    "name":"Blades",
+    "description":"Lifts and rotates when wind is blown over them, causing the rotor to spin. Most turbines have either two or three blades."
+  },
+  {
+    "id":"1",
+    "cls":"brake",
+    "name":"Brake",
+    "description":"Stops the rotor mechanically, electrically, or hydraulically, in emergencies."
+  },
+  {
+    "id":"1",
+    "cls":"controller",
+    "name":"Controller",
+    "description":"Starts up the machine at wind speeds of about 8 to 16 miles per hour (mph) and shuts off the machine at about 55 mph. Turbines do not operate at wind speeds above about 55 mph because they may be damaged by the high winds."
+  },
+  {
+    "id":"1",
+    "cls":"gear-box",
+    "name":"Gear box",
+    "description":"Connects the low-speed shaft to the high-speed shaft and increases the rotational speeds from about 30-60 rotations per minute (rpm), to about 1,000-1,800 rpm; this is the rotational speed required by most generators to produce electricity. The gear box is a costly (and heavy) part of the wind turbine and engineers are exploring \\"
+  },
+  {
+    "id":"1",
+    "cls":"generator",
+    "name":"Generator",
+    "description":"Produces 60-cycle AC electricity; it is usually an off-the-shelf induction generator."
+  },
+  {
+    "id":"1",
+    "cls":"high-speed-shaft",
+    "name":"High-speed shaft",
+    "description":"Drives the generator."
+  },
+  {
+    "id":"1",
+    "cls":"low-speed-shaft",
+    "name":"Low-speed shaft",
+    "description":"Turns the low-speed shaft at about 30-60 rpm."
+  },
+  {
+    "id":"1",
+    "cls":"nacelle",
+    "name":"Nacelle",
+    "description":"Sits atop the tower and contains the gear box, low- and high-speed shafts, generator, controller, and brake. Some nacelles are large enough for a helicopter to land on."
+  },
+  {
+    "id":"1",
+    "cls":"pitch",
+    "name":"Pitch",
+    "description":"Turns (or pitches) blades out of the wind to control the rotor speed, and to keep the rotor from turning in winds that are too high or too low to produce electricity."
+  },
+  {
+    "id":"1",
+    "cls":"rotor",
+    "name":"Rotor",
+    "description":"Blades and hub together form the rotor."
+  },
+  {
+    "id":"1",
+    "cls":"tower",
+    "name":"Tower",
+    "description":"Made from tubular steel (shown here), concrete, or steel lattice. Supports the structure of the turbine. Because wind speed increases with height, taller towers enable turbines to capture more energy and generate more electricity."
+  },
+  {
+    "id":"1",
+    "cls":"wind-direction",
+    "name":"Wind direction",
+    "description":"Determines the design of the turbine. Upwind turbines—like the one shown here—face into the wind while downwind turbines face away."
+  },
+  {
+    "id":"1",
+    "cls":"wind-vane",
+    "name":"Wind vane",
+    "description":"Measures wind direction and communicates with the yaw drive to orient the turbine properly with respect to the wind."
+  },
+  {
+    "id":"1",
+    "cls":"yaw-drive",
+    "name":"Yaw drive",
+    "description":"Orients upwind turbines to keep them facing the wind when the direction changes. Downwind turbines don't require a yaw drive because the wind manually blows the rotor away from it."
+  },
+  {
+    "id":"1",
+    "cls":"yaw-motor",
+    "name":"Yaw motor",
+    "description":"Powers the yaw drive."
+  }
+]
--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// Insert your scripts here
-
 // Initial Script
 
-		var map = L.map('map', {
-			scrollWheelZoom: false
-		}).fitBounds([
-            [42.461, -56.979],[32.536, -134.4]
-            ]);
 
-		var cloudmade = L.tileLayer("http://a.tiles.mapbox.com/v3/energy.map-461t6jk2/{z}/{x}/{y}.png", {
-			//attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-			//key: 'BC9A493B41014CAABB98F0471D759707',
-			//styleId: 22677
-		}).addTo(map);
+(function ($) { 
+
+  $(document).ready(function() { 
+      console.log('hello')
+
+    $('.parts').bind('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+        console.log('hover')
+    });
+
+    // $( "#anemometer" ).mouseover(
+      // console.log('hover')
+      // console.log(this)
+
+    // );
+
+  // $('.hover').bind('touchstart touchend', function(e) {
+  //       e.preventDefault();
+  //       $(this).toggleClass('hover_effect');
+  //   });
+
+
+
+  });  
+}(jQuery));  
